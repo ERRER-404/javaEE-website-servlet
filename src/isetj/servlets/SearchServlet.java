0b5e02a9d2b005bd065fb0e9a8813f2996c58242
MED AZIZ <31410664+ERRER-404@users.noreply.github.com>
2023-03-06 12:31:45 +0100

    Add files via upload

---
+++ src/isetj/servlets/SearchServlet.java
@@ -0,0 +1,45 @@
+package isetj.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SearchServlet")
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+String res=request.getParameter("rechearchname");
+String moteur=request.getParameter("options")	;
+
+		if (moteur.equals("yahoo")){
+			response.sendRedirect("https://www.search.yahoo.com/search?p="+res);
+		}else if (moteur.equals("google"))
+		{
+			response.sendRedirect("https://www.google.com/search?q="+res);		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+	}
+
+}
